ac0b70ab1a6cdd79ef60b1a90a25c9d5e6121f4a
haammar-ledger <hakim.aammar-ext@ledger.fr>
2020-05-22 18:35:27 +0200

    Algorand database I/O for operations

---
+++ ledger-core-algorand/inc/algorand/database/AlgorandOperationDatabaseHelper.hpp
@@ -0,0 +1,60 @@
+/*
+ * AlgorandOperationDatabaseHelper
+ *
+ * Created by Hakim Aammar on 18/05/2020.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Ledger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#ifndef LEDGER_CORE_ALGORANDOPERATIONDATABASEHELPER_H
+#define LEDGER_CORE_ALGORANDOPERATIONDATABASEHELPER_H
+
+#include <algorand/operations/AlgorandOperation.hpp>
+
+#include <core/operation/OperationDatabaseHelper.hpp>
+
+#include <soci.h>
+
+namespace ledger {
+namespace core {
+namespace algorand {
+
+    class OperationDatabaseHelper : public ledger::core::OperationDatabaseHelper {
+
+    public:
+
+        static void putAlgorandOperation(soci::session & sql, const std::string & txUid, const Operation & operation) {
+            sql << "INSERT INTO algorand_operations VALUES(:uid, :tx_uid, :tx_hash)",
+                soci::use(operation.uid),
+                soci::use(txUid),
+                soci::use(operation.getTransaction()->getId();
+        }
+
+    };
+
+} // namespace algorand
+} // namespace core
+} // namespace ledger
+
+#endif // LEDGER_CORE_ALGORANDOPERATIONDATABASEHELPER_H
